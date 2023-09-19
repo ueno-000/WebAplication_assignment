daa9d4deddf1b3bc840265153d051e813e596959
ueno-000 <sarasa4869@zoho.com>
2023-09-19 16:28:01 +0900

    [Update] 課題20完了

--- javakiso_enshu/src/enshu20/MyMain.java
+++ javakiso_enshu/src/enshu20/MyMain.java
@@ -6,29 +6,46 @@ public class MyMain {
 	{
 	
 		String[] names = {"A","B","C","D"};
-		int[] moneys = {500,550,1000,1501};
+		int[] moneys = {500,550,1000,1500};
 		
-		int myMoney;
-		myMoney = 1000;
 		
-
-		System.out.println("元々の自分の所持金："+myMoney+"円");
-		
-		for(int i = 0;i < moneys.length;i++)	
-		{
-			if(myMoney < moneys[i])
-			{
-				moneys[i] -= 100;
-				myMoney += 100;
-				System.out.println(names[i]+"さんから100円貰いました");
-				System.out.println("元々の自分の所持金："+myMoney+"円");
-			}
-		}
 		
 		for(int i = 0;i < moneys.length;i++)
 		{
 			System.out.println(names[i]+"さんの所持金"+moneys[i]+"円");
 		}
 		
+		System.out.println("合計金額:"+sumMoney(moneys)+"円\r\n"+"平均金額:"+aveMoney(moneys)+"円\r\n");
+
+		
+	}
+	
+    /**
+     *合計金額を求める
+     * @param studentMoney[] 所持金配列 
+     * @return 合計金額
+     */
+	private static int sumMoney(int[] studentsMoney) 
+	{
+		int result = 0;
+		for(int i = 0;i < studentsMoney.length;i++)
+		{
+			result += studentsMoney[i];		
+		}
+		return result;
+	}
+    /**
+     *平均金額を求める
+     * @param studentMoney[] 所持金配列 
+     * @return 合計金額
+     */
+	private static double aveMoney(int[] studentsMoney) 
+	{
+		int result = 0;
+		for(int i = 0;i < studentsMoney.length;i++)
+		{
+			result += studentsMoney[i];		
+		}
+		return result/4.0;
 	}
 }
