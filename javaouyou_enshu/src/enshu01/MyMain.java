f2d4c373f9cd8da057bcaf405354d75b0fca7714
ueno-000 <sarasa4869@zoho.com>
2023-09-19 16:45:24 +0900

    [Update] 課題１完了

--- javaouyou_enshu/src/enshu01/MyMain.java
+++ javaouyou_enshu/src/enshu01/MyMain.java
@@ -1,5 +1,14 @@
 package enshu01;
 
-public class MyMain {
+public class MyMain 
+{
+    public static void main(String[] args) 
+    {
+        Store store1 = new Store("東京店", 500000);
+        Store store2 = new Store("渋谷店", 1000000);
 
+        System.out.println(store1.storeName + "の売上：" + store1.sales + "円");
+        System.out.println(store2.storeName + "の売上：" + store2.sales + "円");
+    }
 }
+
---
+++ javaouyou_enshu/src/enshu01/Store.java
@@ -0,0 +1,14 @@
+package enshu01;
+
+public class Store 
+{
+    String storeName;
+    int sales;
+
+    // コンストラクタ
+    public Store(String storeName, int sales) 
+    {
+        this.storeName = storeName;
+        this.sales = sales;
+    }
+}
