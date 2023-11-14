7854117023fd0a8905ed905688feed4e7efe5592
ueno-000 <sarasa4869@zoho.com>
2023-11-14 17:03:52 +0900

    [add] 演習6 フィルタの作成

--- servletjsp/src/enshu06/MenuServlet.java
+++ servletjsp/src/enshu06/MenuServlet.java
@@ -14,23 +14,24 @@ public class MenuServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-        String name = request.getParameter("name");
+ 
         String password = request.getParameter("password");
-        
-        if("password".equals(password))
-        {
-	        response.getWriter().println(
+
+        if ("password".equals(password)) {
+            // 一致する場合、メニュー画面を表示する
+            String name = request.getParameter("name");
+            //response.setContentType("text/html; charset=UTF-8");
+            response.getWriter().println(
 	        		"<html>"
 	        		+ "<body>"
 	        			+ "<h1>メニュー</h1>"
 	        			+ "<h3>ようこそ" + name + "さん</h3>"
 	        		+ "</body>"
 					+ "</html>");
-        }
-        else
-        {
-        	response.sendRedirect("enshu06/error.html");	        	
+        } 
+        else {
+            // 一致しない場合、エラー画面にリダイレクトする
+            response.sendRedirect("enshu06/error.html");
         }
     }
 }
--- servletjsp/src/filter/EncodingFilter.java
+++ servletjsp/src/filter/EncodingFilter.java
@@ -8,26 +8,50 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpFilter;
 
-public class EncodingFilter implements Filter {
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-    	
-    	request.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html;charset=UTF-8");
-        chain.doFilter(request, response);
-        
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter(urlPatterns = { "/*" },initParams = { @WebInitParam(name = "contentType",value = "text/html;charset=UTF-8")} )
+public class EncodingFilter extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public EncodingFilter() {
+        super();
+        // TODO Auto-generated constructor stub
     }
 
+	/**
+	 * @see Filter#destroy()
+	 */
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
 
-    }
-
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
     @Override
-    public void destroy() {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+    	 request.setCharacterEncoding("UTF-8");
+         response.setContentType("text/html; charset=UTF-8");
+         chain.doFilter(request, response);
 
-    }
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+    @Override
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
 }
